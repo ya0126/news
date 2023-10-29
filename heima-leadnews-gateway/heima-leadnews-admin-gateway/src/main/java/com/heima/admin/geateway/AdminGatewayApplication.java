054e372e6c5ce0c5f44c5f68c23161d040876408
yaoh <2160938998@qq.com>
2023-10-29 22:14:09 +0800

    delete

--- heima-leadnews-gateway/heima-leadnews-admin-gateway/src/main/java/com/heima/admin/geateway/AdminGatewayApplication.java
+++ heima-leadnews-gateway/heima-leadnews-admin-gateway/src/main/java/com/heima/admin/geateway/AdminGatewayApplication.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * 后台管理系统引导类
- *
- * @author yaoh
- */
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class AdminGatewayApplication {

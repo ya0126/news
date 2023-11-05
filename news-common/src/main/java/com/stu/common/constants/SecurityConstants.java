fb271e4b473c0a89e36f5387c0ce1222528cedb2
yaoh <2160938998@qq.com>
2023-11-05 15:02:18 +0800

    添加权限相关通用常量

---
+++ news-common/src/main/java/com/stu/common/constants/SecurityConstants.java
@@ -0,0 +1,19 @@
+package com.stu.common.constants;
+
+/**
+ * 权限相关通用常量
+ *
+ * @author yaoh
+ */
+public class SecurityConstants {
+
+    /**
+     * 用户ID字段
+     */
+    public static final String DETAILS_USER_ID = "user_id";
+
+    /**
+     * 授权信息字段
+     */
+    public static final String AUTHORIZATION_HEADER = "token";
+}
